2244e5e04392aab4166d0a3507242fff35b08c03
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-07-29 23:00:25 +0100

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -3,21 +3,23 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     Plugins: [
-      "module-resolver",
-      {
-        root: ["./src"], // This is root directory
-        alias: {
-          "@assets": "./src/assets",
-          "@components": "./src/components",
-          "@theme": "./src/theme",
-          "@constants": "./src/constants",
-          "@hooks": "./src/hooks",
-          "@navigations": "./src/navigations",
-          "@screens": "./src/screens",
-          "@services": "./src/services",
-          "@utils": "./src/utils",
+      [
+        "module-resolver",
+        {
+          root: ["./src"], // This is root directory
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@theme": "./src/theme",
+            "@constants": "./src/constants",
+            "@hooks": "./src/hooks",
+            "@navigations": "./src/navigations",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@utils": "./src/utils",
+          },
         },
-      },
+      ],
     ],
   };
 };
