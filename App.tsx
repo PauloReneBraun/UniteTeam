5fbd3e3fba8db5907a9070e6f05cc9379ddacea4
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-07-30 22:55:47 +0100

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -2,5 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { Groups } from "./src/screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  <ThemeProvider theme={}>
+    <Groups />
+  </ThemeProvider>;
 }
