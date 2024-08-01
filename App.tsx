9c9d62cede6c44ed9d990d1a86668f4cdf46dbe0
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-01 23:15:49 +0100

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar></StatusBar>
       {fontsLoading ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
