295f354296980547fc9f1f7fda7ea6cdf9ee62b2
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-02 21:52:40 +0100

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Loading } from "@components/Loading";
+import { Loading } from "./src/components/Loading";
 
 import theme from "./src/theme";
 
